6c9a94de4f2ee55ebc2f811e35a4f7afadfafdcd
Brendan <brendan.norris@yandy.com>
2018-02-16 01:19:01 +0000

    added css to docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,8 @@ var sass = require('gulp-sass');
 gulp.task('styles', function() {
     gulp.src('./src/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist/'))
+        .pipe(gulp.dest('./docs/'));
 });
 
 gulp.task('watch', function () {
